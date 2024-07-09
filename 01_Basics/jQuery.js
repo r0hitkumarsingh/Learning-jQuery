a15d13cfa53b3db37e14a853d1fef98d32eb6662
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-07-09 08:18:54 +0000

    jQuery

---
+++ 01_Basics/jQuery.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    $('p:first').click(function () {
+        $(this).hide();
+    })
+
+
+    $("btn-one").click(function(){
+        $("#two").hide();
+    })
+
+    // $("#three").mouseleave(function(){
+    //     alert("bye!!!")
+    // })
+
+    // $("three").mouseleave(function () {
+    //     alert("Bye !!!")
+    // })
+
+    $(".box").on({
+        mouseenter: function(){
+            $(this).css("background-color", "#676EF1")
+        },
+
+        mouseleave: function () {
+            $(this).css("background-color", "#fff")
+        },
+
+        click: function(){
+            $(this).css("background-color", "#94535E")
+        }
+    });
+ 
+    $("#btn-two").on({
+        click: function(){
+            $("#div-four").show(2000);
+        }
+    })
+
+    $("#btn-three").on({
+        click: function(){
+            $("#div-four").hide(1000);
+        }
+    })
+
+
+    $(".btn-toggle").click(function(){
+        $("#div-four").toggle(1000);
+    });
+
+});
